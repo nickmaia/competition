bb452ae1f13bef7d62a37733065fc7fa12ff4b76
Isaac Miranda Camargos <isaacmirandacamargos@gmail.com>
2023-01-28 19:13:20 -0300

    feat: Exercise number 1036 beecrowd

---
+++ 1036.py
@@ -0,0 +1,15 @@
+a, b, c = map(float, input().split())
+
+delta = (b**2 -4*a*c)
+
+if delta < 0:
+    print("Impossivel calcular")
+else:
+    try:
+        r1 = (-b+delta**(1/2))/(2*a)
+        r2 = (-b-delta**(1/2))/(2*a)
+        print(f"R1 = {r1:.5f}")
+        print(f"R2 = {r2:.5f}")
+    except:
+        print("Impossivel calcular")
+
